cc0b6983a124c2953931e6635ab861687412a494
shima1203 <kurosaki.shima.631@s.kyushu-u.ac.jp>
2023-09-06 10:32:55 +0900

    processing API data

--- src/weather.js
+++ src/weather.js
@@ -9,11 +9,9 @@ class Weather extends Component {
     }
 
     componentDidMount() {
-        // APIエンドポイントのURL
         const apiUrl1 = 'https://api.weather.com/v3/wx/forecast/hourly/15day?geocode=35.65,135.79&format=json&units=m&language=jaJP&apiKey=7698370dea91420198370dea91720199';
         const apiUrl2 = 'https://api.weather.com/v3/wx/hod/r1/direct?geocode=35.65,135.79&startDateTime=2022-11-01T00Z&endDateTime=2022-11-10T00Z&format=json&units=m&apiKey=7698370dea91420198370dea91720199';
 
-        // APIを呼び出す
         fetch(apiUrl2)
         .then((response) => {
             if (!response.ok) {
@@ -22,14 +20,21 @@ class Weather extends Component {
             return response.json();
         })
         .then((data) => {
-            // 取得したデータをstateにセット
             this.setState({ weatherData: data });
             console.log('Weather Data:', data);
         })
         .catch((error) => {
-            // エラーハンドリング
             this.setState({ error: error });
         });
+    }
+        
+    mkarray = (oneDArray) => {
+        const twoDArray = [];
+        for (let i = 0; i < 9; i++) {
+            twoDArray.push(oneDArray.slice(i*12, (i + 1) * 12));
+        }
+        console.log(twoDArray)
+        return twoDArray;
     }
 
     render() {
@@ -43,12 +48,14 @@ class Weather extends Component {
             return <div>Loading...</div>;
         }
 
+        
         return (
             <div>
                 <h1>Weather Information</h1>
-                <p>Temperature: {weatherData.temperature}</p>
-                <p>Humidity: {weatherData.temperature}</p>
-                <p>Weather: {weatherData.conditions}</p>
+                <p>Temperature: {this.mkarray(weatherData.temperature)}</p>
+
+                <p>Humidity: {weatherData.relativeHumidity}</p>
+                <p>Weather: {weatherData.iconCode}</p>
             </div>
         );
     }
